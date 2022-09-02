47e8568dbe69de8d7ac3094b8c4ef20d64bf4a54
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-09-02 17:07:18 -0400

    Implement config_banks method

--- mcp23017.cpp
+++ mcp23017.cpp
@@ -14,6 +14,20 @@ MCP23017::~MCP23017(void)
     Wire.end();
 }
 
+void MCP23017::config_bank(const uint8_t bank, const uint8_t mode)
+{
+    const uint8_t reg = (bank == BANK_A) ? IODIRA : IODIRB;
+    const uint8_t opmode = (mode == OUTPUT) ? 0 : 1;
+
+    write_reg(reg, opmode);
+}
+
+void MCP23017::config_banks(const uint8_t mode)
+{
+    config_bank(BANK_A, mode);
+    config_bank(BANK_B, mode);
+}
+
 void MCP23017::write_reg(const uint8_t reg, uint8_t value)
 {
     Wire.beginTransmission(_address);
