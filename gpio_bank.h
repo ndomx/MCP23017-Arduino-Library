d70d8ca9531006e3f186a3d7ee76f0eaabe4c311
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-09-02 21:38:33 -0400

    Remove logic from struct

--- gpio_bank.h
+++ gpio_bank.h
@@ -7,22 +7,17 @@
 struct GPIO_Bank
 {
     public:
-    GPIO_Bank(const uint8_t id);
+    GPIO_Bank(const uint8_t id) : reg_mask(id << 4)
+    {
+        ports = 0;
+        ddr = 0;
+        pull_ups = 0;
+    }
 
-    void config(const uint8_t mode, const uint8_t mask);
-
-    void write(const uint8_t mask);
-    uint8_t read(const uint8_t mask);
-    uint8_t read(void);
-
-    private:
-    const uint8_t _reg_mask;
-    uint8_t _ports;
-    uint8_t _ddr;
-    uint8_t _pull_ups;
-
-    void write_reg(const uint8_t reg, uint8_t value);
-    uint8_t read_reg(const uint8_t reg);
+    const uint8_t reg_mask;
+    uint8_t ports;
+    uint8_t ddr;
+    uint8_t pull_ups;
 };
 
 #endif // MCP23017_GPIO_BANK_H_
